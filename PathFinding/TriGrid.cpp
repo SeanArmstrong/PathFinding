55540213eaf32350730e8c997a7580c37d8bc3e1
Sean Armstrong <sw.armstrong@ntlworld.com>
2015-04-10 18:19:17 +0100

    Bitta tidying

--- PathFinding/TriGrid.cpp
+++ PathFinding/TriGrid.cpp
@@ -11,7 +11,6 @@ TriGrid::TriGrid(){
 	}
 }
 
-
 TriGrid::~TriGrid()
 {
 	delete terrainMap;
@@ -27,6 +26,122 @@ void TriGrid::printTerrainMap(){
 
 void TriGrid::loadNewTerrainMap(const std::string filename){
 	terrainMap->loadNewMap(filename);
+}
+
+void TriGrid::FindShortestPath(char startNodeXLetter, int startNodeY, char goalNodeXLetter, int goalNodeY){
+	for (int y = 0; y < YLENGTH; y++){
+		for (int x = 0; x < XLENGTH; x++){
+			nodes[x][y].reset();
+		}
+	}
+	openList.clear();
+	closedList.clear();
+
+
+	TriNode* startNode = nullptr;
+	TriNode* goalNode = nullptr;
+
+	int startNodeX = (int)tolower(startNodeXLetter - 97);
+	int goalNodeX = (int)tolower(goalNodeXLetter - 97);
+
+	startNode = &(nodes[startNodeX][startNodeY]);
+	goalNode = &(nodes[goalNodeX][goalNodeY]);
+
+	if (goalNode && startNode){
+		if (startNode == goalNode){
+			std::cout << "Error: Goal node is same and start node" << std::endl;
+		}
+		else if (goalNode->getCost() < 0){ // Can you start on one. In theory you are going down. Might be ok?
+			std::cout << "Error: Cannot finish on a mountain" << std::endl;
+		}
+		else{
+			AStar(startNode, goalNode);
+		}
+	}
+	else{
+		std::cout << "Could not find goal or start node" << std::endl;
+	}
+}
+
+void TriGrid::AStar(TriNode* startNode, TriNode* goalNode){
+	int goalNodeX = goalNode->getX();
+	int goalNodeY = goalNode->getY();
+
+	bool pathFound = false;
+	startNode->setG(0);
+	startNode->setH(goalNodeX, goalNodeY);
+	startNode->calculateF();
+	openList.insert(startNode);
+	TriNode* currentNode = startNode;
+
+	std::multiset<TriNode*>::iterator openListIt;
+	std::list<TriNode*>::iterator closedListIt;
+
+	while (openList.size() > 0){
+		currentNode = *(openList.begin()); // Set current node to lowest f
+		std::cout << "Current Node: " << currentNode->getId() << std::endl;
+
+		openList.erase(openList.begin()); // Pop from openlist
+		closedList.push_front(currentNode);
+
+		if (currentNode == goalNode){
+			pathFound = true;
+			break;
+		}
+
+		std::vector<TriNode*> neighbours = currentNode->getNeighbours();
+
+		for (int i = 0; i < neighbours.size(); i++){
+			int newGValue = 0;
+			TriNode* comparisonNode = neighbours[i];
+
+
+			if (comparisonNode){
+				if (comparisonNode->getCost() < 0){ // we have a mountain to climb
+					continue;
+				}
+
+				/*** Multiple Lakes ***/
+				if ((terrainMap->getType(comparisonNode->getX(), comparisonNode->getY()) == 'L'&& // Comp node is lake
+					terrainMap->getType(currentNode->getX(), currentNode->getY()) == 'L' && // Current node is lake
+					(currentNode->hasParent() && terrainMap->getType(currentNode->getParent()->getX(), currentNode->getParent()->getY()) == 'L')) && // current node parent is lake
+					comparisonNode != currentNode->getParent()){
+
+					multipleLakes(currentNode, comparisonNode, goalNodeX, goalNodeY);
+
+				}
+				else{
+					newGValue = currentNode->getG() + comparisonNode->getCost();
+					comparisonNode->setH(goalNodeX, goalNodeY);
+					comparisonNode->calculateF();
+
+					openListIt = openList.find(comparisonNode);
+					closedListIt = std::find(closedList.begin(), closedList.end(), comparisonNode);
+					if (openListIt != openList.end() || closedListIt != closedList.end()){ // on the open or closed list
+						if (newGValue < comparisonNode->getG()){
+							//currentNode->setHasADivergeParent(false);
+							//comparisonNode->setHasADivergeParent(false);
+							comparisonNode->setG(newGValue);
+							comparisonNode->calculateF();
+							comparisonNode->setParent(currentNode);
+						}
+					}
+					else{
+						//currentNode->setHasADivergeParent(false);
+						//comparisonNode->setHasADivergeParent(false);
+						comparisonNode->setG(newGValue);
+						comparisonNode->calculateF();
+						comparisonNode->setParent(currentNode);
+						openList.insert(comparisonNode);
+					}
+				}
+			}
+		}
+	}
+
+	if (pathFound == true){
+		printPath(closedList.front());
+	}
 }
 
 void TriGrid::multipleLakes(TriNode* currentNode, TriNode* comparisonNode, const int goalNodeX, const int goalNodeY){
@@ -80,132 +195,9 @@ void TriGrid::multipleLakes(TriNode* currentNode, TriNode* comparisonNode, const
 	}
 }
 
-void TriGrid::AStar(TriNode* startNode, TriNode* goalNode){
-	int goalNodeX = goalNode->getX();
-	int goalNodeY = goalNode->getY();
-	for (int y = 0; y < YLENGTH; y++){
-		for (int x = 0; x < XLENGTH; x++){
-			nodes[x][y].reset();
-		}
-	}
-
-	bool pathFound = false;
-	startNode->setG(0);
-	startNode->setH(goalNodeX, goalNodeY);
-	startNode->calculateF();
-	openList.insert(startNode);
-	TriNode* currentNode = startNode;
-
-	std::multiset<TriNode*>::iterator openListIt;
-	std::list<TriNode*>::iterator closedListIt;
-
-	while (openList.size() > 0){
-		currentNode = *(openList.begin()); // Set current node to lowest f
-		std::cout << "Current Node: " << currentNode->getId() << std::endl;
-
-		openList.erase(openList.begin()); // Pop from openlist
-		closedList.push_front(currentNode);
-
-		if (currentNode == goalNode){
-			pathFound = true;
-			closedList.push_front(currentNode);
-			break;
-		}
-
-		std::vector<TriNode*> neighbours = currentNode->getNeighbours();
-
-		for (int i = 0; i < neighbours.size(); i++){
-			std::cout << "Neighbour: " << i << std::endl;
-
-			int newGValue = 0;
-
-			TriNode* comparisonNode = neighbours[i];
-
-			//if (comparisonNode->hasDivergeParent() && comparisonNode->getDivergeParent() == currentNode){
-			//	continue;
-			//}
-
-			if (comparisonNode){
-				if (comparisonNode->getCost() < 0){ // we have a mountain to climb
-					continue;
-				}
-
-				/*** Multiple Lakes ***/
-				if ((terrainMap->getType(comparisonNode->getX(), comparisonNode->getY()) == 'L'&& // Comp node is lake
-					terrainMap->getType(currentNode->getX(), currentNode->getY()) == 'L' && // Current node is lake
-					(currentNode->hasParent() && terrainMap->getType(currentNode->getParent()->getX(), currentNode->getParent()->getY()) == 'L')) &&
-					comparisonNode != currentNode->getParent()){ // parent is lake
-
-					multipleLakes(currentNode, comparisonNode, goalNodeX, goalNodeY);
-
-				}
-				else{
-					newGValue = currentNode->getG() + comparisonNode->getCost();
-					comparisonNode->setH(goalNodeX, goalNodeY);
-					comparisonNode->calculateF();
-
-					openListIt = openList.find(comparisonNode);
-					closedListIt = std::find(closedList.begin(), closedList.end(), comparisonNode);
-					if (openListIt != openList.end() || closedListIt != closedList.end()){ // on the open or closed list
-						if (newGValue < comparisonNode->getG()){
-							//currentNode->setHasADivergeParent(false);
-							//comparisonNode->setHasADivergeParent(false);
-							comparisonNode->setG(newGValue);
-							comparisonNode->calculateF();
-							comparisonNode->setParent(currentNode);
-						}
-					}
-					else{
-						//currentNode->setHasADivergeParent(false);
-						//comparisonNode->setHasADivergeParent(false);
-						comparisonNode->setG(newGValue);
-						comparisonNode->calculateF();
-						comparisonNode->setParent(currentNode);
-						openList.insert(comparisonNode);
-					}
-				}
-			}
-		}
-	}
-
-	if (pathFound == true){
-		printPath(closedList.front());
-	}
-}
-
-void TriGrid::FindShortestPath(char startNodeXLetter, int startNodeY, char goalNodeXLetter, int goalNodeY){
-	openList.clear();
-	closedList.clear();
-
-
-	TriNode* startNode = nullptr;
-	TriNode* goalNode = nullptr;
-
-	int startNodeX = (int)tolower(startNodeXLetter - 97);
-	int goalNodeX = (int)tolower(goalNodeXLetter - 97);
-
-	startNode = &(nodes[startNodeX][startNodeY]);
-	goalNode = &(nodes[goalNodeX][goalNodeY]);
-
-	if (goalNode && startNode){
-		if (startNode == goalNode){
-			std::cout << "Error: Goal node is same and start node" << std::endl;
-		}
-		else if (goalNode->getCost() < 0){ // Can you start on one. In theory you are going down. Might be ok?
-			std::cout << "Error: Cannot finish on a mountain" << std::endl;
-		}
-		else{
-			AStar(startNode, goalNode);
-		}
-	}
-	else{
-		std::cout << "Could not find goal or start node" << std::endl;
-	}
-}
-
 void TriGrid::printPath(const TriNode* printingNode) const {
 	std::cout << "Path: " << std::endl;
-
+	std::cout << "Cost: " << printingNode->getF() << std::endl;
 	std::cout << "Goal Node: " << printingNode->getId() << " x: " << printingNode->getX() <<
 		", y: " << printingNode->getY() << std::endl;
 
@@ -230,10 +222,6 @@ void TriGrid::printPath(const TriNode* printingNode) const {
 
 	std::cout << "Start Node: " << printingNode->getId() << " x: " << printingNode->getX() <<
 		", y: " << printingNode->getY() << std::endl;
-
-
-
-	std::cout << "Path Cost: " << closedList.front()->getF() << std::endl;
 }
 
 
@@ -287,9 +275,6 @@ void TriGrid::generateGrid(){
 			}
 
 			nodes[x][y] = TriNode(x, y, up, down, left, right, terrainMap);
-			//std::cout << x << " " << y << " " << up << " " << down << " " << left << " " << right << std::endl;
-
 		}
-		//std::cout << std::endl;
 	}
 }
